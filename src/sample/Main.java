14f516d0049ca9f2be784be7c57fc594f5b9a0e7
Daniel Pino <dannyp32@live.com>
2015-05-04 15:13:58 -0700

    Sample JavaFX webview using local html and css files

---
+++ src/sample/Main.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane borderPane = new BorderPane();
+        WebView webView = new WebView();
+        String url = getClass().getResource("test.html").toExternalForm();
+        webView.getEngine().load(url);
+        borderPane.setCenter(webView);
+        final Scene scene = new Scene(borderPane);
+        stage.setScene(scene);
+        stage.setHeight(300);
+        stage.setWidth(250);
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
